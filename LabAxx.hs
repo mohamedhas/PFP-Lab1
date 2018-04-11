d99915dede33acd5d7599fae8cfa6d0ef928424a
moh <moh@moh>
2018-04-11 12:28:36 +0200

    1st commit

---
+++ LabAxx.hs
@@ -0,0 +1,120 @@
+import Data.List
+import Data.Lists
+import System.Random
+import Criterion.Main
+import Control.Parallel
+import Control.Parallel.Strategies as S
+import Control.Monad.Par
+import Test.QuickCheck.Modifiers
+
+--import Data.List.Utils
+
+
+-- code borrowed from the Stanford Course 240h (Functional Systems in Haskell)
+-- I suspect it comes from Bryan O'Sullivan, author of Criterion
+
+data T a = T !a !Int
+
+
+mean :: (RealFrac a) => [a] -> a
+mean = fini . foldl' go (T 0 0)
+  where
+    fini (T a _) = a
+    go (T m n) x = T m' n'
+      where m' = m + (x - m) / fromIntegral n'
+            n' = n + 1
+
+
+resamples :: Int -> [a] -> [[a]]
+resamples k xs =
+    take (length xs - k) $
+    zipWith (++) (inits xs) (map (drop k) (tails xs))
+
+
+jackknife :: ([a] -> b) -> [a] -> [b]
+jackknife f = map f . resamples 500
+
+crud = zipWith (\x a -> sin (x / 300)**2 + a)  [0..]
+
+main = do
+  let (xs,ys) = splitAt 1500  (take 6000
+                               (randoms (mkStdGen 211570155)) :: [Float] )
+  -- handy (later) to give same input different parallel functions
+
+  let rs = crud xs ++ ys
+  putStrLn $ "sample mean:    " ++ show (mean rs)
+
+  let j = jackknife mean rs :: [Float]
+  putStrLn $ "jack mean min:  " ++ show (minimum j)
+  putStrLn $ "jack mean max:  " ++ show (maximum j)
+  defaultMain
+        [
+           bench "jackknife"      (nf (jackknife       mean) rs),
+           --bench "pjackknife"     (nf (pjackknife      mean) rs),
+           --bench "rjackknife"     (nf (rjackknife      mean) rs),
+           bench "sjackknife"     (nf (sjackknife  mean) rs)--,
+           --bench "mjackknife"     (nf (mjackknife  mean) rs)
+         ]
+
+-- 1a
+pmap :: (a -> b) -> [a] -> [b]
+pmap _ []     = []
+pmap f (x:xs) = fx `par` (fxs `pseq` (fx : fxs))
+  where
+    fx = f x
+    fxs = pmap f xs
+
+pjackknife :: ([a] -> b) -> [a] -> [b]
+pjackknife f = pmap f . resamples 500
+
+-- 1b
+rmap :: (a -> b) -> [a] -> [b]
+rmap _ []     = []
+rmap f (x:xs) = runEval $ do
+    fx  <- rpar (f x)
+    fxs <- rseq (rmap f xs)
+    return (fx:fxs)
+
+rjackknife :: ([a] -> b) -> [a] -> [b]
+rjackknife f = rmap f . resamples 500
+{-
+sjackknife :: ([a] -> b) -> [a] -> [b]
+sjackknife f xs = ((parList $ r0 f) `dot` (r0 $ resamples 500 )) xs
+-}
+
+
+{-
+sjackknife :: ([a] -> b) -> [a] -> [b]
+sjackknife f xs = S.parMap rpar f $ resamples 500 xs
+-}
+
+sjackknife :: (NFData b) => ([a] -> b) -> [a] -> [b]
+sjackknife f xs = using list (parListChunk 100 rdeepseq)
+    where list = jackknife f xs
+
+mjackknife :: (NFData b) => ([a] -> b) -> [a] -> [b]
+mjackknife f xs = runPar $ parMapM (return . f) $ resamples 500 xs
+
+split :: (NFData a) => [a] -> ([a], [a])
+split myList = splitAt (((length myList) + 1) `div` 2) myList
+
+mergeSort :: (Ord a, NFData a) => [a] -> [a]
+mergeSort xs = case Main.split xs of
+        ([], [])       -> []
+        (x:[], [])     -> [x]
+        (sp1, sp2) -> runPar $ do
+              i <- new
+              j <- new
+              fork (put i (mergeSort sp1))
+              fork (put j (mergeSort sp2))
+              rs1 <- get i
+              rs2 <- get j
+              return $ merge rs1 rs2
+
+mergeSort2 :: (Ord a, NFData a) => [a] -> [a]
+mergeSort2 xs = case Main.split xs of
+        ([], [])       -> []
+        (x:[], [])     -> [x]
+        (sp1, sp2) -> par rs1 $ par rs2 $ pseq rs1 $ pseq rs2 merge rs1 rs2
+                      where rs1 = mergeSort2 sp1
+                            rs2 = mergeSort2 sp2
