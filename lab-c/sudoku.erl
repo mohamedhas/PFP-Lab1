76fae355b706eafdabaa8c5fb189b9453026d753
mohamedhas <d.mohmspb@gmail.com>
2018-05-18 15:12:46 +0200

    fixing refine

--- lab-c/sudoku.erl
+++ lab-c/sudoku.erl
@@ -87,83 +87,43 @@ on_exit(Fun) ->
       {'EXIT',Pid,Why} -> Fun(Why)
     end.
 
-<<<<<<< HEAD
-
+receiveM(Ref) ->
+  receive
+    no_solution ->
+      exit(no_solution);
+    {Ref, Solution} ->
+      Solution
+  end.
 
 
 handle_recive(M) ->
-=======
-refineM(Manager, Ref, M) ->
-  Manager ! {Ref,
-    refine_rows(
-      transpose(
-        refine_rows(
-          transpose(
-            unblocks(
-              refine_rows(
-                blocks(M)))))))}.
-
-manager(Parent,M) ->
-  process_flag(trap_exit,true), %%don't let mngr die bc of wrkr?
-  Manager = self(),
-  Ref1 = make_ref(),
-  Ref2 = make_ref(),
-  Ref3 = make_ref(),
-  %spawn_link(sudoku, refineM, [Parent, M]),
-  Worker1 = spawn_link(sudoku, refineM, [Manager, Ref1, (lists:sublist(M,1,3))]),
-  Worker2 = spawn_link(sudoku, refineM, [Manager, Ref2, (lists:sublist(M,4,3))]),
-  Worker3 = spawn_link(sudoku, refineM, [Manager, Ref3, (lists:sublist(M,7,3))]),
-  case manager_recieve(Ref1, Ref2, Ref3, Parent) of
-  	{Ref1, Top} -> ?????? ;
-  	{Ref2, Mid} -> ?????? ; %%Lol this doesn't work yet, still figuring it out
-  	{Ref3, Bot} -> ??????
-  end.
-  Parent ! {Top ++ Mid ++ Bot}.
-
-  %on_exit( (fun(T) -> Parent ! {xt, T} end)).
-
-manager_recieve(Ref1, Ref2, Ref3, Parent) ->
-    receive
-      {'EXIT',Pid,Why} -> Parent ! {xt, Why};
-      {Ref, Mpartial} -> {Ref, Mpartial} end.
-
-      %{Ref2, M2} -> {Ref2, M2} end;
-      %{Ref3, M3} -> {Ref3, M3} end;
-	%Top = receive {Ref1, M1} -> M1 end,
-	%Mid = receive {Ref2, M2} -> M2 end,
-	%Bot = receive {Ref3, M3} -> M3 end,		
-	
-
-%the parent will call this to recieve message of solutions form manager
-parent_recive(M) ->
->>>>>>> 9de6d3c3ae863a3b292753564705166ab807cd1e
   receive
     {xt, no_solution} -> io:format("***exit: no_solution \n",[]), exit(no_solution);
-    {xt, Ms} -> io:format("***exit: ~p\n",[Ms]), parent_recive(M);
+    {xt, Ms} -> io:format("***exit: ~p\n",[Ms]), handle_recive(M);
     Xs -> if M==Xs ->
       M;
             true ->
               refine(Xs)
           end end.
 
-<<<<<<< HEAD
-=======
-refine(M) ->
-  spawn_link(sudoku, manager, [self(), M]),
-  parent_recive(M).
->>>>>>> 9de6d3c3ae863a3b292753564705166ab807cd1e
 
 
-%%sum_(Pid) -> Pid ! (2 + 2).
-%%
-%%test_sum() ->
-%%  Parent = self(),
-%%  spawn_link(sudoku, sum_, [Parent]),
-%%  receive X -> X end.
+sum_(Pid) -> Pid ! (2 + 2).
+
+test_sum() ->
+  Parent = self(),
+  spawn_link(sudoku, sum_, [Parent]),
+  receive X -> X end.
 
 %%fun() -> Parent ! (2+2) end
 
 refine_rows(M) ->
+  whereis(manager1) ! {self(), M},
+  NewM = receiveM('ref1') ++ receiveM('ref2'),
+  io:format("***Matrix: ~p\n",[NewM]),
+  NewM.
+
+prefine_rows(M) ->
   lists:map(fun refine_row/1,M).
 
 
@@ -188,20 +148,14 @@ refine_rows(M) ->
 
 
 worker() ->
-  receive {Parent, M} ->
-    case catch refine_rows(
-        transpose(
-          refine_rows(
-            transpose(
-              unblocks(
-                refine_rows(
-                  blocks(M))))))) of
+  receive {Ref, Parent, M} ->
+    case catch prefine_rows(M) of
         {'EXIT',no_solution} ->
           Parent ! no_solution;
       {'EXIT',Ms} -> io:format("***--exit: ~p\n",[Ms])
         ;
         Solution ->
-            Parent ! Solution
+            Parent ! {Ref, Solution}
       end
   end,
   worker().
@@ -210,25 +164,28 @@ manager() ->
   %%process_flag(trap_exit,true),
   %%spawn_link(sudoku, refineM, [Parent, M]),
   receive
-    {Parent, M} -> whereis(worker1) ! {Parent, lists:sublist(M,1,5)},
-                   whereis(worker2) ! {Parent, lists:sublist(M,6,4)}
+    {Parent, M} -> whereis(worker1) ! {'ref1', Parent, lists:sublist(M,1,5)},
+                   whereis(worker2) ! {'ref2', Parent, lists:sublist(M,6,4)}
   end,
   manager().
   %%on_exit( (fun(T) -> Parent ! {xt, T} end)).
 
-receiveM() ->
-  receive
-    no_solution ->
-      exit(no_solution);
-    Solution ->
-      Solution
-  end.
+
 
 refine(M) ->
-  whereis(manager1) ! {self(), M},
-  receiveM() ++ receiveM() .
-
-
+  NewM =
+    refine_rows(
+      transpose(
+        refine_rows(
+          transpose(
+            unblocks(
+              refine_rows(
+                blocks(M))))))),
+  if M==NewM ->
+    M;
+    true ->
+      refine(NewM)
+  end.
 
 refine_row(Row, Parent) ->
   Entries = entries(Row),
@@ -377,8 +334,8 @@ solve_one([M|Ms]) ->
   end.
 
 initPs() ->
-  register(worker1, spawn_link(sudoku, worker,[])),
-  register(worker2, spawn_link(sudoku, worker,[])),
+  register(worker1,  spawn_link(sudoku, worker,[])),
+  register(worker2,  spawn_link(sudoku, worker,[])),
   register(manager1, spawn_link(sudoku, manager,[])).
 
 
